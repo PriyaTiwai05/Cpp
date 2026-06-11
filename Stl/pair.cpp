8c515875ffc6a15163702a72c0b24e98ce343846
Priya Raj <priyati162505@gmail.com>
2026-06-11 11:34:18 +0530

    stl learning

---
+++ Stl/pair.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <deque>
+using namespace std;
+
+int main(){
+  pair<string,int>p={"priya",20};
+  cout<<p.first<<endl;
+  cout<<p.second<<endl;
+    return 0;
+}
